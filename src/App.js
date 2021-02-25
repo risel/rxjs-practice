ad532bcc9749e7734c1e8b338ec47fd3c891f773
danteYoon <dante@watcha.com>
2021-02-26 00:36:10 +0900

    Update: use interval

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useEffect, useRef, useState } from "react";
-import { of } from "rxjs";
+import { interval } from "rxjs";
+import { map } from "rxjs/operators";
 
 const source = [ "Dante", "Peter", "James"];
-const names$ = of(source);
+const names$ = interval(1000).pipe(map(i => source.slice(0, i+1)));
 
 function App() {
   const [names, setNames] = useState();
@@ -12,13 +13,13 @@ function App() {
     return () => subscription.unsubscribe();
   },[]);  
   const handleList = () => {
-    return names.map((item) => {
+    return names?.map((item) => {
       return <div>{item}</div>
     })
   }
   return (
     <div>
-      {handleList}
+      {handleList()}
     </div>
   );
 }
